31abfcbf5e1007157e999d5b23d0620ec3ceb644
shuva93-dass <shuva93@gmail.com>
2019-10-21 19:37:04 -0500

    Update Agent.py

--- RNN-Actor Critic/Agent.py
+++ RNN-Actor Critic/Agent.py
@@ -72,3 +72,46 @@ class Agent:
             self.learn(experiences) 
             self.last_state = next_state   
     
+    def learn(self, experiences):   
+        #Extracting the states,actions,etc from all the experience tuples
+        states = np.vstack([e.state for e in experiences if e is not None]).astype(np.float32).reshape(-1,self.state_size)    
+        actions = np.vstack([e.action for e in experiences if e is not None]).astype(np.float32).reshape(-1,self.action_size)
+        rewards = np.array([e.reward for e in experiences if e is not None]).astype(np.float32).reshape(-1,1)
+        dones = np.array([e.done for e in experiences if e is not None]).astype(np.float32).reshape(-1,1)
+        next_states = np.vstack([e.next_state for e in experiences if e is not None]).astype(np.float32).reshape(-1,self.state_size) 
+
+        
+        #Reshaping all the vectors into 3-dimensional vector to be fed into LSTM architecture
+        states = np.reshape(states, (states.shape[0],states.shape[1],1))
+        next_states = np.reshape(next_states, (next_states.shape[0],next_states.shape[1],1))
+        rewards = np.reshape(rewards, (rewards.shape[0],rewards.shape[1],1))
+        dones = np.reshape(dones, (dones.shape[0],dones.shape[1],1))
+        actions = np.reshape(actions, (actions.shape[0],actions.shape[1],1))
+        
+        #return a separate array for each exp and predict actions based on next states
+        actions_next = self.actor_target.model.predict_on_batch(next_states)
+        
+        #Reshaping the vector
+        actions_next = np.reshape(actions_next,(actions_next.shape[0],actions_next.shape[1],1))
+       
+        #predict qvalues for actor o/p for the next state
+        Q_targets_next = self.critic_target.model.predict_on_batch([next_states, actions_next]) 
+        
+        #target the q-value to serve as label for critic model based on temporal diff
+        Q_targets = rewards + self.gamma * Q_targets_next * (1 - dones)
+        
+        #fit the critic model to the time difference of the target
+        Q_targets = np.reshape(Q_targets, (Q_targets.shape[0],Q_targets.shape[1],1))        
+        self.critic_local.model.train_on_batch(x = [states, actions], y = Q_targets) 
+        
+        action_gradients = np.reshape(self.critic_local.get_action_gradients([states, actions, 0]),(-1, self.action_size))
+        self.actor_local.train_fn([states, action_gradients, 1]) 
+        self.soft_update(self.actor_local.model, self.actor_target.model)
+        
+    def soft_update(self, local_model, target_model):
+        local_weights = np.array(local_model.get_weights())
+        target_weights = np.array(target_model.get_weights())
+        assert len(local_weights) == len(target_weights)
+        new_weights = self.tau * local_weights + (1 - self.tau) * target_weights
+        target_model.set_weights(new_weights)
+
