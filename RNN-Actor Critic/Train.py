adcec92c4662d846b9c51218364bed7e9d904b8c
VKPriya <48071424+VKPriya@users.noreply.github.com>
2019-10-22 23:00:20 -0500

    Update Train.py

--- RNN-Actor Critic/Train.py
+++ RNN-Actor Critic/Train.py
@@ -1,4 +1,3 @@
-#Train script
 import sys
 import datetime
 import pytz 
@@ -7,14 +6,11 @@ window_size = 100
 batch_size = 32
 agent = Agent(window_size, batch_size)  
 data = df.iloc[:,4].tolist()
-#print('daata',len(data))
 l = len(data) -1
-#l = l-1
-episode_count = 300
+episode_count = 100
 
 train_start = datetime.datetime.now(pytz.timezone('America/Chicago')) 
-
-print ('\033[95m'+'Start time for training:',train_start.strftime("%I:%M:%S %p"))
+print ('\033[95m'+'Start time for training:',train_start.strftime("%I:%M:%S %p") + '\033[0m')
 
 for e in range(episode_count):
   
@@ -26,7 +22,6 @@ for e in range(episode_count):
 
     for t in range(l):
         action = agent.act(np.reshape(state,(state.shape[0],state.shape[1],1)))
-        print("action:",action)
         
         state = np.reshape(state,(state.shape[0],state.shape[1],1))   
         action_prob = agent.actor_local.model.predict(state)
@@ -35,14 +30,14 @@ for e in range(episode_count):
         
         if action == 1:
             agent.inventory.append(data[t])
-            print('\033[94m'+"Buy the stock at Price:" + formatPrice(data[t]))
+            print('\033[94m'+"Buy the stock at Price:" + formatPrice(data[t]) + '\033[0m')
         elif action == 2 and len(agent.inventory) > 0:  # sell
             bought_price = agent.inventory.pop(0)
             reward = max(data[t] - bought_price, 0)
             total_profit += data[t] - bought_price
-            print('\033[91m' +'Sell the stock at Price: '+ formatPrice(data[t]) + ' | '  + 'Updated Profit: ' + '\033[1m'+ formatPrice(data[t] - bought_price))
+            print('\033[91m' +'Sell the stock at Price: '+ formatPrice(data[t]) + ' | '  + 'Updated Profit: ' + '\033[1m'+ formatPrice(data[t] - bought_price)+ '\033[0m')
         else:
-            print ('\033[1m' + 'Hold the Stock')
+            print ('\033[0m' + '\033[1m' + 'Hold the Stock'+ '\033[0m')
 
         if t == l - 1:
             done = True
@@ -51,13 +46,10 @@ for e in range(episode_count):
 
         if done:
             epi_end = datetime.datetime.now(pytz.timezone('America/Chicago')) 
-            print('\033[1m' + '------------------------------------------')
-            print('\033[1m' + 'Total Profit for  epoch in RNN: ' + formatPrice(total_profit))
-            print ('\033[95m' + 'end train time for episode {} : {}:'.format(e,epi_end.strftime("%I:%M:%S %p")))
-            print('\033[1m' + '------------------------------------------')
-            
-            
-            
-        
-train_end = datetime.datetime.now(pytz.timezone('America/Chicago'))
-print ('\033[95m'+ 'End time for training:',train_end.strftime("%I:%M:%S %p"))
+            print('\033[1m' + '------------------------------------------' + '\033[0m')
+            print('\033[1m' + 'Total Profit for  epoch in RNN: ' + formatPrice(total_profit) + '\033[0m')
+            print ('\033[95m' + 'end train time for episode {} : {}:'.format(e,epi_end.strftime("%I:%M:%S %p"))+ '\033[0m')
+            print('\033[1m' + '------------------------------------------' + '\033[0m')
+     
+train_end = datetime.datetime.now(pytz.timezone('America/Chicago')) 
+print ('\033[95m'+ 'Start time for training:',train_end.strftime("%I:%M:%S %p")+ '\033[0m')
